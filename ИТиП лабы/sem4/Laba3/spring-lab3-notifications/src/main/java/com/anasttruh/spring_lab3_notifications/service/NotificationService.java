e792b8c62e30829172ddcb6599cb417dc29c8e4b
trukh <trukhina12345@mail.ru>
2026-04-28 19:16:10 +0300

    first

--- ИТиП лабы/sem4/Laba3/spring-lab3-notifications/src/main/java/com/anasttruh/spring_lab3_notifications/service/NotificationService.java
+++ ИТиП лабы/sem4/Laba3/spring-lab3-notifications/src/main/java/com/anasttruh/spring_lab3_notifications/service/NotificationService.java
@@ -87,3 +87,28 @@ public class NotificationService {
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+public NotificationService(NotificationRepository notificationRepository) {
+    this.notificationRepository = notificationRepository;
+}
+ */
